9cbdee5a3539d546e915e1ae5a2af7ccd54b50e8
mukesh22777 <shriramtecgno@gmail.com>
2020-05-18 14:11:15 -0400

    18  May Update

--- src/test/java/LoginpageTest/loginpage.java
+++ src/test/java/LoginpageTest/loginpage.java
@@ -5,9 +5,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -20,47 +18,38 @@ public class loginpage {
 		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\Lib\\Drivers\\Newchromedriver\\chromedriver.exe");
 		driver = new ChromeDriver();
 
-		driver.get("https://www.phptravels.com/");
+		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.findElement(By.xpath("//a[@class='login']")).click();
 		Thread.sleep(2000);
 	}
 
 	@Test
+	
 	public void title() throws InterruptedException {
 		String my_title = driver.getTitle();
 		System.out.println("Home page Title is " + my_title);
-		String Expected_title = "PHPTRAVELS booking script and system for hotels airline flights tours cars online application - PHPTRAVELS";
+		String Expected_title = "Login - My Store";
 		Assert.assertEquals(my_title, Expected_title);
+		Thread.sleep(5000);
 	}
 
-	@Test
-	public void logoImageTEst() {
-		WebElement ImageFile = driver.findElement(By.xpath("//img[@class='brand']"));
-
-		Boolean ImagePresent = (Boolean) ((JavascriptExecutor) driver).executeScript(
-				"return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0",
-				ImageFile);
-		if (!ImagePresent) {
-			System.out.println("Logo is not displayed.");
-		} else {
-			System.out.println("Logo is displayed.");
-		}
-	}
+	
 
 	@Test
-	public void companyTab_contect_us() throws InterruptedException {
-		// click on company tab
-		driver.findElement(By.xpath("//*[@class='has-drop']//span[text()='Company']")).click();
-		// click on contect us link
-		driver.findElement(By.xpath("/html/body/header/div[2]/div/nav/ul/li[5]/div/ul/li[5]/a")).click();
-		// Display Addressh
-		Boolean Display = driver.findElement(By.xpath("//*[@class='panel-heading']")).isDisplayed();
-		// To print the value true or falls
-		System.out.println("Element displayed is :" + Display);
+	public void verify_signin() throws InterruptedException {
+		
+		// click on email
+		driver.findElement(By.xpath("//form[@id=\"login_form\"]//input[@id='email']")).sendKeys("shriramtechno@gmail.com");
+		// click on password
+		driver.findElement(By.id("passwd")).sendKeys("Mukesh@1977");
+		// click on Sign in button
+		driver.findElement(By.xpath("//button[@id='SubmitLogin']")).click();
+		
 		Thread.sleep(4000);
 	}
-
+	
 	@AfterMethod
 	public void afterMethod() {
 		driver.quit();
